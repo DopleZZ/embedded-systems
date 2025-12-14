d9b574673ac9e5f47c5f4a6ff2d951a5e29b56cd
DopleZZ <tema.bond@outlook.com>
2025-12-14 20:32:47 +0300

    enum fx

--- backend/src/main/java/com/fitocube/backend/model/enums/FriendRequestStatus.java
+++ backend/src/main/java/com/fitocube/backend/model/enums/FriendRequestStatus.java
@@ -1,7 +1,20 @@
 package com.fitocube.backend.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum FriendRequestStatus {
-    PENDING,
-    ACCEPTED,
-    REJECTED
+    PENDING("pending"),
+    ACCEPTED("accepted"),
+    REJECTED("rejected");
+
+    private final String value;
+
+    FriendRequestStatus(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
